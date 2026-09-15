3a137674f285c0e25729a408d2df4b4aa73ee8ee
Sahriz <94785672+Sahriz@users.noreply.github.com>
2026-09-15 15:33:40 +0200

    Updated message

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args){
         KeyPair keys = RSA.generateKeyPair(); //Generate both the private key and the public key used for encryption and decryption.
 
-        String message = "Jag äslkar dig!    s"; //message to encrypt and decrypt
+        String message = "Hello, World! There's mice on venus? Goddness."; //message to encrypt and decrypt
 
         BigInteger cipherTextValue = RSA.encrypt(message, keys.publicKey); //encryption
         String recoveredString = RSA.decrypt(cipherTextValue, keys.privateKey); //decryption
